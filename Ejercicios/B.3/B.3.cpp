bab34e3658a2177b0fa0990547603fca8335f140
jorgep6 <149598412+jorgep6@users.noreply.github.com>
2025-11-24 14:36:16 +0100

    Refactor rellenarMatriz function and update logic

--- Ejercicios/B.3/B.3.cpp
+++ Ejercicios/B.3/B.3.cpp
@@ -1,7 +1,7 @@
 
 /*@ <authors>
  *
- * Nombre, apellidos y usuario del juez (TAISXXX) de los autores de la solución.
+ * Jorge Perez Ramirez F32 Alejandro Orgaz FernÃ¡ndez F29
  *
  *@ </authors> */
 
@@ -10,78 +10,69 @@
 using namespace std;
 
 #include "Matriz.h"  // propios o los de las estructuras de datos de clase
-
 /*@ <answer>
 
- Escribe aquí un comentario general sobre la solución, explicando cómo
- se resuelve el problema y cuál es el coste de la solución, en función
- del tamaño del problema.
+ Escribe aquÃ­ un comentario general sobre la soluciÃ³n, explicando cÃ³mo
+ se resuelve el problema y cuÃ¡l es el coste de la soluciÃ³n, en funciÃ³n
+ del tamaÃ±o del problema.
 
  @ </answer> */
 
 
  // ================================================================
- // Escribe el código completo de tu solución aquí debajo
+ // Escribe el cÃ³digo completo de tu soluciÃ³n aquÃ­ debajo
  // ================================================================
  //@ <answer>
 
-int rellenarMatriz(Matriz<int>& matriz, const vector<int>& comidas, int cubI, int cubD ) {
-	if (matriz[cubI][cubD] == -1) {
-		if (cubI == cubD) 
-			matriz.at(cubI, cubD) = comidas.at(cubI - 1);
-		
-		else if (cubI > cubD) 
-			matriz.at(cubI, cubD) = 0;
-			
+int rellenarMatriz(vector<int> const& cubos, int const& i, int const& j, Matriz<int> & m) {
+	if (i >= cubos.size() || j < 0)
+		return 0;
+	
+	int& res = m[i][j];
+	if (res == -1) {
+		if (i > j)
+			res = 0;
+		else if (i == j)
+			res = cubos[i];
 		else {
-			int aux1 = cubI + 1;
- 			int aux2 = cubD;
-			if (comidas.at(aux1 - 1) > comidas.at(aux2 - 1))
-				aux1++;
-			else
-				aux2--;
-			int izq = rellenarMatriz(matriz, comidas, aux1, aux2) + comidas.at(cubI - 1);
+			
+			int sol1 = cubos[i], sol2 = cubos[j];
 
-			aux1 = cubI;
-			aux2 = cubD - 1;
-			if (comidas.at(aux1 - 1) > comidas.at(aux2 - 1))
-				aux1++;
-			else
-				aux2--;
-			int der = rellenarMatriz(matriz, comidas, aux1, aux2) + comidas.at(cubD - 1);
+			sol1 += cubos[i + 1] >= cubos[j] ? rellenarMatriz(cubos, i + 2, j, m) : rellenarMatriz(cubos, i + 1, j - 1, m);
 
-			matriz.at(cubI, cubD) = max(izq, der);
+			sol2 += cubos[i] >= cubos[j - 1] ? rellenarMatriz(cubos, i + 1, j - 1, m) : rellenarMatriz(cubos, i, j - 2, m);
+
+			res = max(sol1, sol2);
 		}
 	}
-	return matriz[cubI][cubD];
-
+	return res;
 }
 
 bool resuelveCaso() {
 	// leer los datos de la entrada
-	int cubos;
-
-	cin >> cubos;
-	if (cubos == 0)
+	int C;
+	cin >> C;
+	if (C == 0)
 		return false;
 
+	vector<int> cubos(C);
+	for (int& a : cubos) {
+		cin >> a;
+	}
 
-	vector<int> comidas(cubos);
-	for (int& i : comidas)
-			cin >> i;
-	
-	Matriz<int> matriz(cubos + 1, cubos + 1, -1);
-	// escribir la solución
+	Matriz<int> m(C, C, -1);
 
-	int sol = rellenarMatriz(matriz, comidas, 1, cubos);
+	int sol = rellenarMatriz(cubos, 0, C - 1, m);
+	// resolver el caso posiblemente llamando a otras funciones
 
 	cout << sol << "\n";
+	// escribir la soluciÃ³n
 
 	return true;
 }
 
 //@ </answer>
-//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
+//  Lo que se escriba dejado de esta lÃ­nea ya no forma parte de la soluciÃ³n.
 
 int main() {
 	// ajustes para que cin extraiga directamente de un fichero
